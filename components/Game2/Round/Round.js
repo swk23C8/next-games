c717e7150cc4e0a66d9b2acf333edd97bf3e4445
swk23c8 <35516289+swk23C8@users.noreply.github.com>
2022-03-28 17:02:12 +0900

    keemstar

--- components/Game2/Round/Round.js
+++ components/Game2/Round/Round.js
@@ -7,7 +7,9 @@ const Round = () => {
 	return (
 		<div className="round">
 			<h1>React Dice Roll</h1>
-			<Dice cheatValue={2} />
+			<Dice cheatValue={2} size={80}/>
+
+			<Dice onRoll={(value) => console.log(value)} size={80}/>
 		</div>
 	);
 };
