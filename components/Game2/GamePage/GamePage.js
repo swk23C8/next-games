bb700aac87d72bec2fbc9fbf8ba88c2ccf90d92a
swk23c8 <35516289+swk23C8@users.noreply.github.com>
2022-03-28 16:20:44 +0900

    lets get this bread

--- components/Game2/GamePage/GamePage.js
+++ components/Game2/GamePage/GamePage.js
@@ -3,7 +3,7 @@ import { useState,  } from 'react';
 import styles from './GamePage.module.scss';
 
 import Round from '../Round/Round';
-import Container from '../Container/Container';
+import Container from '../../Container/Container';
 
 const GamePage = () => {
   const [isGameStarted, setIsGameStarted] = useState(false);
