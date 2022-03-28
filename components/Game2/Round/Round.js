48a4e137efae3bc3f87b05a1a6275be153b71550
swk23c8 <35516289+swk23C8@users.noreply.github.com>
2022-03-29 01:43:24 +0900

    simple roll combination checker for cee-lo

--- components/Game2/Round/Round.js
+++ components/Game2/Round/Round.js
@@ -38,6 +38,57 @@ import styles from './Round.module.scss';
 
 // export default Round;
 
+// return roll combination of dice
+// INSTANT WIN
+// triple: All 3 dice are the same, 6-6-6 is the highest roll and 5-5-5 is the next highest and so on
+// 4-5-6: Rolling 4-5-6 is an instant win
+// x-x-6: Rolling a pair plus 6 is an instant win
+// POINTS
+// x-x-y: Rolling a pair plus any other value is a point where the odd die is the point value
+// INSTANT LOSS
+// x-x-1: Rolling a pair plus 1 is an instant loss
+// 1-2-3: Rolling 1-2-3 is an instant loss
+// INTERMEDIATE
+// x-y-z: Any combination that does not result in cases above is an intermediate outcome and requires a re-roll
+const rollCombination = (dice) => {
+	// sort dice
+	dice.sort((a, b) => a - b);
+	// console.log("dice:", dice);
+	// check for triple
+	if (dice[0] === dice[1] && dice[1] === dice[2]) {
+		return "triple";
+	}
+	// check for 4-5-6
+	if (dice[0] === 4 && dice[1] === 5 && dice[2] === 6) {
+		return "instantWin";
+	}
+	// check for x-x-6
+	if (dice[0] === dice[1] && dice[2] === 6) {
+		return "instantWin";
+	}
+	// check for x-x-y
+	if (dice[0] === dice[1] && dice[2] !== 6) {
+		return "point";
+	}
+	// check for x-x-1
+	if (dice[0] === dice[1] && dice[2] === 1) {
+		return "instantLoss";
+	}
+	// check for 1-2-3
+	if (dice[0] === 1 && dice[1] === 2 && dice[2] === 3) {
+		return "instantLoss";
+	}
+	// check for x-y-z
+	if (dice[0] !== dice[1] && dice[1] !== dice[2] && dice[0] !== dice[2]) {
+		return "intermediate";
+	}
+	// return default
+	return "default";
+	// return dice.reduce((acc, curr) => acc + curr, 0);
+};
+
+
+
 export default function Round() {
 	return (
 		<div className={styles.round}>
