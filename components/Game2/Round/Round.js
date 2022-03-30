b40ad0196c539629266aaa93c3c0f9d79f93e3f2
swk23c8 <35516289+swk23C8@users.noreply.github.com>
2022-03-31 01:44:44 +0900

    almost done

--- components/Game2/Round/Round.js
+++ components/Game2/Round/Round.js
@@ -61,11 +61,13 @@ const rollCombination = (dice) => {
 	// return dice.reduce((acc, curr) => acc + curr, 0);
 };
 
-const score = (dice) => {
+const score = (dice, currentPlayer) => {
+	console.log(currentPlayer);
+
 	dice.sort();
 	// check if die values are null
 	if (dice[0] === null || dice[1] === null || dice[2] === null) {
-		return 'null';
+		return -2;
 	}
 	if (dice[0] === dice[1] && dice[1] === dice[2]) return 10;
 	if (dice.join() === "4,5,6") return 10;
@@ -81,29 +83,42 @@ const score = (dice) => {
 // class Round extends Component {
 const Round = () => {
 
-	// Banker dice useStates
+	const score = (dice) => {
+		dice.sort();
+		// check if die values are null
+		if (dice[0] === null || dice[1] === null || dice[2] === null) {
+			return -2;
+		}
+		if (dice[0] === dice[1] && dice[1] === dice[2]) return 10;
+		if (dice.join() === "4,5,6") return 10;
+		if (dice[0] !== dice[1] && dice[1] !== dice[2] && dice[1] !== dice[3]) return 0;
+		if (dice.join() === "1,2,3") return -1;
+		if (dice[0] === dice[1] || dice[1] === dice[2] || dice[1] === dice[3]) {
+			const pointDie = dice[0] === dice[1] ? dice[2] : dice[0];
+			return pointDie === 1 ? -1 : pointDie === 6 ? 10 : pointDie;
+		}
+	}
+
+	// Banker useStates
 	const [bDie_1, setBDie_1] = useState(null);
 	const [bDie_2, setBDie_2] = useState(null);
 	const [bDie_3, setBDie_3] = useState(null);
+	const [bScore, setBScore] = useState(null);
+	const [bRoll, setBRoll] = useState(false);
 
-	// Player dice useStates
+	// Player useStates
 	const [pDie_1, setPDie_1] = useState(null);
 	const [pDie_2, setPDie_2] = useState(null);
 	const [pDie_3, setPDie_3] = useState(null);
+	const [pScore, setPScore] = useState(null);
+	const [pRoll, setPRoll] = useState(false);
+
 
 
 	useEffect(() => {
-		// POINTS: x-x-3 
-		// console.log(score([5, 5, 3]));
-		// console.log(score([2, 6, 6]));
-		// console.log(score([6, 6, 6]));
-		// console.log(score([4, 5, 6]));
-		// console.log(score([3, 3, 6]));
-		// console.log(score([4, 4, 5]));
-		// console.log(score([5, 5, 1]));
-		// console.log(score([3, 5, 6]));
-		// console.log(score([1, 1, 1]));
-	}, []);
+		setPScore(score([pDie_1, pDie_2, pDie_3]))
+		setBScore(score([bDie_1, bDie_2, bDie_3]))
+	}, [bDie_1, bDie_2, bDie_3, pDie_1, pDie_2, pDie_3]);
 
 	return (
 		<div className={styles.round}>
@@ -124,8 +139,8 @@ const Round = () => {
 				size={80}
 				disabled={bDie_3 !== null} />
 			{console.log("banker dice:", bDie_1, bDie_2, bDie_3)}
-			{console.log(score([bDie_1, bDie_2, bDie_3]))}
-
+			{console.log("score:", bScore)}
+			<h3>{bScore == -2 ? "Roll Bank's dice" : "score: " + bScore}</h3>
 
 			<h2>Player</h2>
 			<Dice
@@ -141,8 +156,8 @@ const Round = () => {
 				size={80}
 				disabled={pDie_3 !== null} />
 			{console.log("player dice:", pDie_1, pDie_2, pDie_3)}
-			{console.log(score([pDie_1, pDie_2, pDie_3]))}
-
+			{console.log("score:", pScore)}
+			<h3>{pScore == -2 ? "Roll Player's dice" : "score: " + pScore}</h3>
 
 			<div></div>
 			{/* button to clear dice value */}
