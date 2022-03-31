5ff9cd26d0607bc04a7f5ea5270b1d1a540e4f23
swk23c8 <35516289+swk23C8@users.noreply.github.com>
2022-03-31 19:41:29 +0900

    removed nibba word

--- components/Game2/GamePage/GamePage.js
+++ components/Game2/GamePage/GamePage.js
@@ -16,7 +16,7 @@ const GamePage = () => {
     <Round setIsGameStarted={setIsGameStarted} />
   ) : (
     <Container>
-      <h1>Cee-Lo: the hood niggas dice game</h1>
+      <h1>Cee-Lo: New Yorker&apos;s favorite dice game</h1>
       <p>Let{"'"}s play?</p>
       <button type="button" onClick={startGame}>
         Start
